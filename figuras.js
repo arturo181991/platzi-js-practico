f8803af664c0939bfcedf0e4f2a759b81f63f624
Arturo Agudelo <arturoagudelo18@gmail.com>
2021-08-30 11:10:58 -0500

    cambios a figura triangulo

--- figuras.js
+++ figuras.js
@@ -1,1 +1,42 @@
-console.log("Hola mundo2..");
+//codigo del cuadrado
+console.group("Cuadrado");
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + " cms");
+
+const permimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado mide: " + permimetroCuadrado + " cms");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado mide: " + areaCuadrado + " cms2");
+console.groupEnd();
+
+console.group("Triangulo");
+//codigo del triangulo
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+console.log("Los lados del triangulo miden: "
+ + ladoTriangulo1
+ + " cms, "
+ + ladoTriangulo2
+ + " cms, "
+ + baseTriangulo
+ +" cms"
+);
+
+const alturaTriangulo = 5.5;
+console.log("La altura del triangulo mide: " + alturaTriangulo + " cms");
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+console.log("El perimetro del triangulo mide: " + perimetroTriangulo + " cms");
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+console.log("El Area del triangulo es de: " + areaTriangulo + " cms2");
+
+console.groupEnd();
+/*const permimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado mide: " + permimetroCuadrado + " cms");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado mide: " + areaCuadrado + " cms2");*/
